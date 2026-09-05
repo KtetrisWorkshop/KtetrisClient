5dc70a74a6041197f6d8bc29f8fa87273a9498e6
Neptrue-Lin <29837162+Neptrue-Lin@users.noreply.github.com>
2026-09-05 22:38:14 +0800

    feat(core): Add moveDown manipulation

---
+++ src/gameplay/core/manipulation.ts
@@ -0,0 +1,10 @@
+import { Board } from "./board";
+import { FallingPiece } from "./piece";
+
+export function moveDown(piece: FallingPiece, board: Board): FallingPiece {
+    if (board.landsOnSurface(piece)) {
+        return piece;
+    } else {
+        return { ...piece, y: piece.y + 1 }
+    }
+}
