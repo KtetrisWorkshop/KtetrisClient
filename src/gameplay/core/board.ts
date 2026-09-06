bf992ce0f3efce9e0a3c9037d40cb71e04e53d5a
Neptrue-Lin <29837162+Neptrue-Lin@users.noreply.github.com>
2026-09-06 21:29:47 +0800

    feat(manipulation): Complete  movement

--- src/gameplay/core/board.ts
+++ src/gameplay/core/board.ts
@@ -1,10 +1,14 @@
 import { Bit, BitField } from "../common/bitfield";
+import { MoveDirection } from "./manipulation";
 import { FallingPiece } from "./piece";
 
 export class Board {
     private readonly playfield: BitField;
 
-    public constructor(width: number, height: number) {
+    public constructor(
+        public readonly width: number,
+        public readonly height: number
+    ) {
         this.playfield = new BitField(width, height, Bit.Zero);
     }
 
@@ -16,7 +20,26 @@ export class Board {
         return this.playfield.indexAt(x, y) == Bit.One;
     }
 
-    public landsOnSurface(piece: FallingPiece): boolean {
-        return this.playfield.mask([piece.x, piece.y], piece.tetromino[piece.facing]).isZero();
+    public touchesFrame(piece: FallingPiece, direction: MoveDirection) { switch (direction) {
+        case MoveDirection.Down: return piece.y == 0;
+        case MoveDirection.Left: return piece.x + direction == 0;
+        case MoveDirection.Right: return piece.x + direction + piece.tetromino[piece.facing].width == this.width - 1;
+    }}
+
+    public touchesBlock(piece: FallingPiece, direction: MoveDirection) {
+        const tetromino = piece.tetromino[piece.facing];
+        const maxX = tetromino.width - 1;
+        const maxY = tetromino.height - 1;
+        switch (direction) {
+            case MoveDirection.Down: return this.playfield.mask([piece.x, piece.y + maxY - 1],
+                tetromino.slice([0, maxY], [maxX, maxY])
+            )
+            case MoveDirection.Left: return this.playfield.mask([piece.x - 1, piece.y],
+                tetromino.slice([0, 0], [0, maxY])
+            )
+            case MoveDirection.Right: return this.playfield.mask([piece.x + maxX + 1, piece.y],
+                tetromino.slice([maxX, 0], [maxX, maxY])
+            )
+        }
     }
 }
--- src/gameplay/core/manipulation.ts
+++ src/gameplay/core/manipulation.ts
@@ -1,10 +1,14 @@
 import { Board } from "./board";
 import { FallingPiece } from "./piece";
 
-export function moveDown(piece: FallingPiece, board: Board): FallingPiece {
-    if (board.landsOnSurface(piece)) {
+export const enum MoveDirection { Left = -1, Down = 0, Right = 1 }
+
+export function move(piece: FallingPiece, direction: MoveDirection, board: Board): FallingPiece {
+    if (board.touchesFrame(piece, direction) || board.touchesBlock(piece, direction)) {
         return piece;
-    } else {
-        return { ...piece, y: piece.y - 1 }
+    } else switch (direction) {
+        case MoveDirection.Down: return { ...piece, y: piece.y - 1 }
+        case MoveDirection.Left: return { ...piece, x: piece.x - 1 }
+        case MoveDirection.Right: return { ...piece, x: piece.x + 1 }
     }
 }
